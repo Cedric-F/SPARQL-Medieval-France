c7192d24bd54e45bd718003aa95768f869253fde
Cedric-F <cedric.fontaine@gmx.fr>
2023-01-08 16:25:35 +0100

    Updates

---
+++ precache-manifest.23eeb61c550d08eef7ddf6ffd2740633.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ce08baa54cdcd7cb5e9a7b34ecc5f8",
+    "url": "/Medieval-France/index.html"
+  },
+  {
+    "revision": "d024e0d3aed9506adfe4",
+    "url": "/Medieval-France/static/css/2.00186c68.chunk.css"
+  },
+  {
+    "revision": "9e558a008f3f7e23a732",
+    "url": "/Medieval-France/static/css/main.4ae63963.chunk.css"
+  },
+  {
+    "revision": "d024e0d3aed9506adfe4",
+    "url": "/Medieval-France/static/js/2.d8093264.chunk.js"
+  },
+  {
+    "revision": "d195b97fa67e0ed5f53cfaac98bbc69a",
+    "url": "/Medieval-France/static/js/2.d8093264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e558a008f3f7e23a732",
+    "url": "/Medieval-France/static/js/main.9600eae7.chunk.js"
+  },
+  {
+    "revision": "88e96e04e2d84678a736",
+    "url": "/Medieval-France/static/js/runtime-main.d890a984.js"
+  },
+  {
+    "revision": "aa9ae202452ea1b48b610408706073a6",
+    "url": "/Medieval-France/static/media/Cardinal.aa9ae202.woff"
+  },
+  {
+    "revision": "c26c8752e7a7abd14a6beeaf7260365a",
+    "url": "/Medieval-France/static/media/Castle.c26c8752.webp"
+  },
+  {
+    "revision": "36b2a5638706fd87118867d49359f4c1",
+    "url": "/Medieval-France/static/media/Cathedral.36b2a563.webp"
+  },
+  {
+    "revision": "0ea4fa966a0018f88e48eb53d2b7d7f4",
+    "url": "/Medieval-France/static/media/City.0ea4fa96.webp"
+  },
+  {
+    "revision": "c672dd53103a8dcace4465bf03ffdf66",
+    "url": "/Medieval-France/static/media/Map-Border.c672dd53.webp"
+  },
+  {
+    "revision": "d2df1b989c73b997c3197462ad58f825",
+    "url": "/Medieval-France/static/media/Side.d2df1b98.webp"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Medieval-France/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Medieval-France/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Medieval-France/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Medieval-France/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Medieval-France/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
